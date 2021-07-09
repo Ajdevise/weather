f1acf6ea6d87a71f57164f3c744517c48f083c37
Ajdevise <ajdevise151@hotmail.com>
2021-07-09 08:48:27 +0200

    added communication with higlights section

--- src/app/forecast/todays-highlights/todays-highlights.component.ts
+++ src/app/forecast/todays-highlights/todays-highlights.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherApiService } from 'src/app/services/weather-api.service';
 
 @Component({
   selector: 'app-todays-highlights',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todays-highlights.component.scss']
 })
 export class TodaysHighlightsComponent implements OnInit {
+  weather: any;
 
-  constructor() { }
+  constructor(private weatherApi: WeatherApiService) { }
 
   ngOnInit() {
+    this.weather = this.weatherApi.getForecastData()[0];
   }
 
 }
